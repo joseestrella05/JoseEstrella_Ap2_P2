9b0f7faa2982c678a3cc2a1b23f1e609981a12a2
Jose Gabriel <Jose Gabriel@DESKTOP-SQIUKTF>
2025-10-29 18:43:39 -0400

    implementacion de hilt

---
+++ app/src/main/java/edu/ucne/jose_estrella_ap2_p2/Jose_Estrella_AP2_P2App.kt
@@ -0,0 +1,7 @@
+package edu.ucne.jose_estrella_ap2_p2
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class Jose_Estrella_AP2_P2App : Application(){}
