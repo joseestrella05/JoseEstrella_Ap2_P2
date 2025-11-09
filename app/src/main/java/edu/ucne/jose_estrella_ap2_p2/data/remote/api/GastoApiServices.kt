aefebcb97c2a1dda0665bb09b1bf188b7258dcd9
Jose Gabriel <Jose Gabriel@DESKTOP-SQIUKTF>
2025-11-09 17:56:34 -0400

    Agregue los end point put y get

--- app/src/main/java/edu/ucne/jose_estrella_ap2_p2/data/remote/api/GastoApiServices.kt
+++ app/src/main/java/edu/ucne/jose_estrella_ap2_p2/data/remote/api/GastoApiServices.kt
@@ -4,10 +4,16 @@ import edu.ucne.jose_estrella_ap2_p2.data.remote.dto.GastoDto
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Path
 
 interface GastoApiServices{
     @GET("api/Gastos")
     suspend fun getGasto() : List<GastoDto>
     @POST("api/Gastos")
     suspend fun postGasto(@Body gasto: GastoDto)
+    @GET("api/Gastos/{id}")
+    suspend fun getGastoById(@Path("id") id: Int): GastoDto
+    @PUT("api/Gastos/{id}")
+    suspend fun putGasto(@Path("id") id: Int, @Body gasto: GastoDto)
 }
