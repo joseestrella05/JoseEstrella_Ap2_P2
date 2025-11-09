33e228c9e07a3d421e0b4868679ac9a0e64e6b52
Jose Gabriel <Jose Gabriel@DESKTOP-SQIUKTF>
2025-11-09 18:00:10 -0400

    Agregue dos nuevo use case GetGastoByIdUseCase.kt y UpdateGastoUseCase.kt

---
+++ app/src/main/java/edu/ucne/jose_estrella_ap2_p2/domain/use_case/GetGastoByIdUseCase.kt
@@ -0,0 +1,10 @@
+package edu.ucne.jose_estrella_ap2_p2.domain.use_case
+
+import edu.ucne.jose_estrella_ap2_p2.domain.model.Gasto
+import edu.ucne.jose_estrella_ap2_p2.domain.repository.GastoRepository
+
+class GetGastoByIdUseCase(private val repository: GastoRepository) {
+    suspend operator fun invoke(id: Int): Gasto {
+        return repository.getGastoById(id)
+    }
+}
