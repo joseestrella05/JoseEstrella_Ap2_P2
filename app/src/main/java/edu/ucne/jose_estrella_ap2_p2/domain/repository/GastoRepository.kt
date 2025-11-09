86a2f7c2b0c5830bc19bd6749759ffca66055cec
Jose Gabriel <Jose Gabriel@DESKTOP-SQIUKTF>
2025-11-09 17:58:04 -0400

    Agregue getGastoById y updateGasto a GastoRepository.kt

--- app/src/main/java/edu/ucne/jose_estrella_ap2_p2/domain/repository/GastoRepository.kt
+++ app/src/main/java/edu/ucne/jose_estrella_ap2_p2/domain/repository/GastoRepository.kt
@@ -5,4 +5,6 @@ import edu.ucne.jose_estrella_ap2_p2.domain.model.Gasto
 interface GastoRepository{
     suspend fun getGasto(): List<Gasto>
     suspend fun  postGasto(gasto: Gasto)
+    suspend fun getGastoById(id: Int): Gasto
+    suspend fun updateGasto(id: Int, gasto: Gasto)
 }
